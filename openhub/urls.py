84f317e6ae1acae1cc28ae1b474dfda71cacd941
Sachin <in-sachins@Sachins-MacBook-Pro.local>
2018-11-22 19:52:12 +0530

    Fix CI Build

--- openhub/urls.py
+++ openhub/urls.py
@@ -10,4 +10,3 @@ urlpatterns = [
     url(r'^api/office-dist-data/$', views.OfficeDistribution.as_view()),
     url(r'^api/git-stats/$', views.get_github_stats, name='git-data')
 ]
-// commented to break CI
